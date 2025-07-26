0b177f2674f176208990c2e93f3ac50eb944a999
Diah082 <152632332+Diah082@users.noreply.github.com>
2025-07-26 16:05:08 +0700

    Update sshws.sh

--- sshws.sh
+++ sshws.sh
@@ -3,7 +3,7 @@ curl -sS ipv4.icanhazip.com > /usr/bin/.ipvps
 REPO="https://raw.githubusercontent.com/Diah082/Ws-Epro/refs/heads/main/"
 wget -O /usr/bin/ws "${REPO}ws"
 wget -O /usr/bin/config.conf "${REPO}config.conf"
-wget -O /etc/systemd/system "${REPO}ws.service"
+wget -O /etc/systemd/system/ws.service "${REPO}ws.service"
 chmod +x /usr/bin/ws
 systemctl daemon-reload
 systemctl enable ws.service
